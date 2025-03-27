c7f45f62d26358b1fc564891115b9ac13642ba13
hseun <hseun0732@dms.hs.kr>
2025-03-27 11:25:22 +0900

    🌱 :: redis

---
+++ src/redis.js
@@ -0,0 +1,11 @@
+const { createClient } = require("redis");
+const redisCli = new createClient({ legacyMode: true });
+
+redisCli.on("connect", () => {
+  console.log("Redis has initted");
+});
+redisCli.on("error", (err) => {
+  console.error(err);
+});
+
+module.exports = { redisCli };
