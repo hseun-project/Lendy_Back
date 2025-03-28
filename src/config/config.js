53c5b3eef1f8a9b8cde50d7522a6df7a7613e492
hseun <hseun0732@dms.hs.kr>
2025-03-28 11:17:11 +0900

    🔧 :: port name 수정

--- src/config/config.js
+++ src/config/config.js
@@ -7,6 +7,6 @@ module.exports = {
   password: process.env.DB_PASSWORD,
   database: process.env.DB_SCHEMA,
   host: process.env.DB_HOST,
-  port: process.env.PORT,
+  port: process.env.DB_PORT,
   dialect: "mysql"
 };
