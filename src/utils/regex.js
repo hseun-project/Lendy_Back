bcedb4abd9760a1c39580630325a8d079e4294e0
hseun <hseun0732@dms.hs.kr>
2025-03-28 14:52:36 +0900

    🌱 :: mailRegex

--- src/utils/regex.js
+++ src/utils/regex.js
@@ -3,6 +3,12 @@ const checkUserIdRegex = (userId) => {
   return userIdPattern.test(userId);
 };
 
+const checkMailRegex = (mail) => {
+  const mailPattern = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+  return mailPattern.test(mail);
+};
+
 module.exports = {
-  checkUserIdRegex
+  checkUserIdRegex,
+  checkMailRegex
 };
