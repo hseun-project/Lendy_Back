10c664f662e3402df5888667c77c44d3adb45a76
hseun <hseun0732@dms.hs.kr>
2025-05-09 11:11:49 +0900

    add :: data

--- src/service/auth/token.ts
+++ src/service/auth/token.ts
@@ -1,6 +1,13 @@
 import { signJWT } from '../../utils/jwt';
 
-export const generateToken = async (id: string, isAccess: boolean) => {
-  const token = signJWT({ id }, isAccess ? '1h' : '7d');
+export const generateToken = (id: string, isAccess: boolean) => {
+  const token = signJWT(
+    {
+      id,
+      type: isAccess ? 'access' : 'refresh',
+      iat: Math.floor(Date.now() / 1000)
+    },
+    isAccess ? '1h' : '7d'
+  );
   return token;
 };
