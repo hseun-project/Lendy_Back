e0e4f091c277f3d24965ae189aa38169434db632
hseun <hseun0732@dms.hs.kr>
2025-05-20 09:02:49 +0900

    add :: try-catch & refresh

--- src/service/open/token.ts
+++ src/service/open/token.ts
@@ -1,4 +1,3 @@
-import { Request, Response } from 'express';
 import { prisma } from '../../config/prisma';
 import redis from '../../config/redis';
 import axios from 'axios';
@@ -11,55 +10,94 @@ const tokenApiUrl = `${openApiTestUrl}/oauth/2.0/token`;
 
 export const userToken = async (userCode: string) => {
   const redirectionUrl = process.env.OPEN_API_REDIRECTION_URL;
-  const res = await axios.post(
-    tokenApiUrl,
-    qs.stringify({
-      code: userCode,
-      client_id: clientId,
-      client_secret: clientSecret,
-      redirect_uri: redirectionUrl,
-      grant_type: 'authorization_code'
-    }),
-    {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        Accept: 'application/json'
+  try {
+    const res = await axios.post(
+      tokenApiUrl,
+      qs.stringify({
+        code: userCode,
+        client_id: clientId,
+        client_secret: clientSecret,
+        redirect_uri: redirectionUrl,
+        grant_type: 'authorization_code'
+      }),
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Accept: 'application/json'
+        }
       }
-    }
-  );
+    );
 
-  const { access_token, refresh_token, expires_in, user_seq_no } = res.data;
+    const { access_token, refresh_token, expires_in, user_seq_no } = res.data;
 
-  const userDetail = await prisma.userDetail.update({
-    where: { userCode: userCode },
-    data: { userSeqNo: user_seq_no }
-  });
+    const userDetail = await prisma.userDetail.update({
+      where: { userCode: userCode },
+      data: { userSeqNo: user_seq_no }
+    });
 
-  await redis.set(`open ${userDetail.id}`, access_token, 'EX', expires_in);
-  await redis.set(`open ${refresh_token}`, `${userDetail.id}`);
+    await redis.set(`openAccess ${userDetail.id}`, access_token, 'EX', expires_in);
+    await redis.set(`openRefresh ${userDetail.id}`, refresh_token, 'EX', expires_in + 10000);
+  } catch (err) {
+    console.error(err);
+    return;
+  }
 };
 
 export const oddToken = async () => {
   const scope = 'oob';
   const grantType = 'client_credentials';
 
-  const res = await axios.post(
-    tokenApiUrl,
-    qs.stringify({
-      client_id: clientId,
-      client_secret: clientSecret,
-      scope: scope,
-      grant_type: grantType
-    }),
-    {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        Accept: 'application/json'
+  try {
+    const res = await axios.post(
+      tokenApiUrl,
+      qs.stringify({
+        client_id: clientId,
+        client_secret: clientSecret,
+        scope: scope,
+        grant_type: grantType
+      }),
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Accept: 'application/json'
+        }
       }
-    }
-  );
+    );
 
-  const { access_token, expires_in, client_use_code } = res.data;
-  await redis.set('lendy', client_use_code, 'EX', expires_in);
-  await redis.set(client_use_code, access_token, 'EX', expires_in);
+    const { access_token, expires_in, client_use_code } = res.data;
+    await redis.set('lendy', client_use_code, 'EX', expires_in);
+    await redis.set(client_use_code, access_token, 'EX', expires_in);
+  } catch (err) {
+    console.error(err);
+    return;
+  }
+};
+
+export const refresh = async (refreshToken: string) => {
+  try {
+    const res = await axios.post(
+      tokenApiUrl,
+      qs.stringify({
+        client_id: clientId,
+        client_secret: clientSecret,
+        refresh_token: refreshToken,
+        scope: 'login inquiry transfer',
+        grant_type: 'refresh_token'
+      })
+    );
+
+    const { access_token, refresh_token, expires_in, user_seq_no } = res.data;
+
+    const userDetail = await prisma.userDetail.findUnique({ where: { userSeqNo: user_seq_no } });
+    if (!userDetail) {
+      console.error('userDetail not found');
+      return;
+    }
+
+    await redis.set(`openAccess ${userDetail.id}`, access_token, 'EX', expires_in);
+    await redis.set(`openRefresh ${userDetail.id}`, refresh_token, 'EX', expires_in + 10000);
+  } catch (err) {
+    console.error(err);
+    return;
+  }
 };
